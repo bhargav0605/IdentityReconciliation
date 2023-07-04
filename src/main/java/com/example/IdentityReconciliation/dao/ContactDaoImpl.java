14cc25614a58ce7a13d6d2c90a3131631b2150ac
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-04 14:08:41 +0530

    Created Data Access Object file which contains queries

--- src/main/java/com/example/IdentityReconciliation/dao/ContactDaoImpl.java
+++ src/main/java/com/example/IdentityReconciliation/dao/ContactDaoImpl.java
@@ -1,17 +1,23 @@
 package com.example.IdentityReconciliation.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class ContactDaoImpl {
-	
-	@Autowired
-	NamedParameterJdbcTemplate template;
-	
-	public void getContact() {
-		
-	}
+import com.example.IdentityReconciliation.model.Contact;
 
+@Repository
+public class ContactDaoImpl{
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@SuppressWarnings("unchecked")
+	public List<Contact> getContact() {
+		String hql = "SELECT c FROM Contact c";
+		return (List<Contact>) entityManager.createQuery(hql).getResultList();
+	}
 }
