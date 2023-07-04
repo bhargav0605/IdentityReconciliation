85589e3a7a3c5a7a529760311721a4aee7c850a3
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-04 14:44:44 +0530

    Created encapsulated data access objects

--- src/main/java/com/example/IdentityReconciliation/dao/ContactDaoImpl.java
+++ src/main/java/com/example/IdentityReconciliation/dao/ContactDaoImpl.java
@@ -10,14 +10,22 @@ import org.springframework.stereotype.Repository;
 import com.example.IdentityReconciliation.model.Contact;
 
 @Repository
-public class ContactDaoImpl{
+public class ContactDaoImpl implements ContactDao{
 	
 	@PersistenceContext
 	private EntityManager entityManager;
-	
+
 	@SuppressWarnings("unchecked")
+	@Override
 	public List<Contact> getContact() {
+		// TODO Auto-generated method stub
 		String hql = "SELECT c FROM Contact c";
 		return (List<Contact>) entityManager.createQuery(hql).getResultList();
 	}
+	
+//	@SuppressWarnings("unchecked")
+//	public List<Contact> getContact() {
+//		String hql = "SELECT c FROM Contact c";
+//		return (List<Contact>) entityManager.createQuery(hql).getResultList();
+//	}
 }
