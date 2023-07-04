0a1092b9d7f66adc0c2d8bb7ffbb3c29ff1a794b
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-04 14:45:17 +0530

    Dao interface implementation

---
+++ src/main/java/com/example/IdentityReconciliation/dao/ContactDao.java
@@ -0,0 +1,11 @@
+package com.example.IdentityReconciliation.dao;
+
+import java.util.List;
+
+import com.example.IdentityReconciliation.model.Contact;
+
+public interface ContactDao {
+	
+	List<Contact> getContact();
+
+}
