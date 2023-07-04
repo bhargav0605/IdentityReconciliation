dc2d45e65f35d2db0b4f3b3e53e95f44256e0c45
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-04 14:42:44 +0530

    Created Interface to encapsulate service (business logic)

--- src/main/java/com/example/IdentityReconciliation/service/IdentityService.java
+++ src/main/java/com/example/IdentityReconciliation/service/IdentityService.java
@@ -2,19 +2,10 @@ package com.example.IdentityReconciliation.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.example.IdentityReconciliation.dao.ContactDaoImpl;
 import com.example.IdentityReconciliation.model.Contact;
 
-@Service
-public class IdentityService {
+public interface IdentityService {
 	
-	@Autowired
-	private ContactDaoImpl contactDaoImpl;
-	
-	public List<Contact> identify() {
-		return contactDaoImpl.getContact();
-	}
+	public List<Contact> identify();
+
 }
