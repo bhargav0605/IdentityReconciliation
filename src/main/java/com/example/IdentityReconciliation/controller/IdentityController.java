b25fa3ecf5ae74012ca5986eac3cf92e711ff437
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-04 14:46:19 +0530

    Code change to use service implementation

--- src/main/java/com/example/IdentityReconciliation/controller/IdentityController.java
+++ src/main/java/com/example/IdentityReconciliation/controller/IdentityController.java
@@ -4,22 +4,25 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.IdentityReconciliation.dto.IdentityRequest;
 import com.example.IdentityReconciliation.model.Contact;
-import com.example.IdentityReconciliation.service.IdentityService;
+import com.example.IdentityReconciliation.service.IdentityServiceImpl;
 
 @RestController
 @RequestMapping("/api")
 public class IdentityController {
 	
 	@Autowired
-	private IdentityService identityService;
+	private IdentityServiceImpl identityService;
 	
 	
 	@PostMapping("/identify")
-	public List<Contact> hello() {
+	public List<Contact> identify(@RequestBody IdentityRequest identityRequest) {
+		System.out.println(identityRequest);
 		return identityService.identify();
 	}
 }
