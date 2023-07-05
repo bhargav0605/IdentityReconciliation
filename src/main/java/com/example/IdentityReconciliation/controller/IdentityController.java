1c34338f922694424e536346fc2ab626d35849c1
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-05 18:10:48 +0530

    Changed the return type

--- src/main/java/com/example/IdentityReconciliation/controller/IdentityController.java
+++ src/main/java/com/example/IdentityReconciliation/controller/IdentityController.java
@@ -4,7 +4,11 @@
  */
 package com.example.IdentityReconciliation.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,19 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.IdentityReconciliation.dto.IdentityRequest;
 import com.example.IdentityReconciliation.dto.IdentityResponse;
+import com.example.IdentityReconciliation.exception.MissingParameterException;
 import com.example.IdentityReconciliation.service.IdentityServiceImpl;
+
+
 
 @RestController
 @RequestMapping("/api")
 public class IdentityController {
 	
+	public static Logger logger = LoggerFactory.getLogger(IdentityController.class);
+	
 	@Autowired
 	private IdentityServiceImpl identityService;
 	
 	@PostMapping("/identify")
-	public IdentityResponse identify(@RequestBody IdentityRequest identityRequest) {
-		IdentityResponse identityResponse = identityService.identify(identityRequest);
-		return identityResponse;
+	public ResponseEntity<Object> identify(@RequestBody IdentityRequest identityRequest) throws MissingParameterException{
+//		System.out.println(identityRequest);
+		IdentityResponse identityResponse = null;
+		String errorMessage = new String();
+		try {
+			if(identityRequest == null) {
+				System.out.println("Empty");
+			}
+			identityResponse = identityService.identify(identityRequest);
+			return new ResponseEntity<Object>(identityResponse, HttpStatus.OK);
+			
+		} catch (MissingParameterException e) {
+			logger.error(e.getMessage());
+			errorMessage = e.getMessage();
+		}
+		return new ResponseEntity<Object>(errorMessage, HttpStatus.BAD_REQUEST);
 	}
-
 }
