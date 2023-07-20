58dc922a0fba113f9088db7fc08e2efce4451947
Bhargav Parmar <bhargavparmar7080@gmail.com>
2023-07-20 16:01:16 +0530

    Tag is changed from only Date to DateTime

--- runit.bat
+++ runit.bat
@@ -6,14 +6,24 @@ set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
 echo Building Jar.
 call mvn clean package -DskipTests
 
-set "datestamp=%YYYY%%MM%%DD%"
-echo datestamp: "%datestamp%"
+set "datestamp=%YYYY%%MM%%DD%" & set "timestamp=%HH%%Min%%Sec%"
+
+::echo Set docker credentials.
+::set DOCKER_USERNAME=<Your-username>
+::set DOCKER_TOKEN=<Your-token>
+
+::echo datestamp: "%datestamp%"
+::echo timestamp: "%timestamp%"
+echo tag: %datestamp%%timestamp%
 
 echo Build docker image.
-call docker build -t identity_reconciliation:"%datestamp%" .
+call docker build -t identity_reconciliation:%datestamp%%timestamp% .
 
 echo retag the docker image.
-call docker tag identity_reconciliation:"%datestamp%" bhargav0605/identity_reconciliation:"%datestamp%"
+call docker tag identity_reconciliation:%datestamp%%timestamp% %DOCKER_USERNAME%/identity_reconciliation:%datestamp%%timestamp%
 call docker images
-call docker push bhargav0605/identity_reconciliation:"%datestamp%"
+
+echo Docker login.
+call docker login --username %DOCKER_USERNAME% --password %DOCKER_TOKEN%
+call docker push bhargav0605/identity_reconciliation:%datestamp%%timestamp%
 pause
